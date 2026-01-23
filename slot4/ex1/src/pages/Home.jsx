d4ff0d52c8ca5ec3bbda4d3105f982518bb85acb
LE ANH TUAN 180303 <leanhtuan.18.03.03@gmail.com>
2026-01-23 12:49:39 +0700

    update page Home layout

--- slot4/ex1/src/pages/Home.jsx
+++ slot4/ex1/src/pages/Home.jsx
@@ -3,21 +3,24 @@ import Banner from '../components/Banner'
 import Menu from '../components/Menu'
 import BookTable from '../components/BookTable'
 import Footer from '../components/Footer'
+import { Container } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <>
-      <Navigation />
-      <Banner />
-      <Menu />
-      <BookTable />
-      <Footer
-        info={{
-          src: 'https://i.pravatar.cc/150?img=1',
-          name: 'John Doe',
-          email: 'john.doe@example.com',
-        }}
-      />
+      <Container>
+        <Navigation />
+        <Banner />
+        <Menu />
+        <BookTable />
+        <Footer
+          info={{
+            src: 'https://i.pravatar.cc/150?img=1',
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+          }}
+        />
+      </Container>
     </>
   )
 }
